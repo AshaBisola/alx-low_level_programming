191af8a19124d463b6519ced02f5406254420035
Ashaye Bisola <ashayebisola@.com>
2023-04-20 21:44:05 -0700

    3&4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
 *main - Entry point
-*Return: Alaways 0 (Sucess)
+*
+*Return: Alaways 0 (Success)
 */
-
 int main (void)
 {
-	puts("\"Proggraming is like building a multilingual puzzle")
-	 	return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+        return (0);
 }		
