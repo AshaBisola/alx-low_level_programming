c23de36ee24592ff01405f3eb5bfb06a2b6e621f
Ashaye Bisola <ashayebisola@.com>
2023-04-20 21:36:07 -0700

    3&4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Alaways 0 (Sucess)
+*/
+
+int main (void)
+{
+	puts("\"Proggraming is like building a multilingual puzzle")
+	 	return (0);
+}		
