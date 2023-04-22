c5f0158bc2554c47e9b53ebbd7e5cbbf71f711bc
Ashaye Bisola <ashayebisola@.com>
2023-04-21 18:32:55 -0700

    ty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
-	#include <time.h>
-	
+/**
+ * main - function to check sign of number.
+ *
+ * Return: 0
+ */
 
-	/**
-	 * main - function to check sign of number.
-	 *
-	 * Return: 0
-	 */
-	
+int main(void)
+{
+	int n;
 
-	int main(void)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
-		int n;
-	
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-	
-
-		if (n > 0)
-		{
-			printf("%d is positive\n", n);
-		}
-		if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
-		if (n < 0)
-		{
-			printf("%d is negative\n", n);
-		}
-	
-
-		return (0);
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
 	}
 
+	return (0);
+}
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <time.h>
-	
 
-	/**
-	 * main - function to check the size of last digit of random
-	 * number
-	 *
-	 * Return: 0
-	 */
-	
+/**
+ * main - function prints all lower case alphas using putchar
+ *
+ * Return: 0
+ */
 
-	int main(void)
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		int n, l;
-	
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-	
-
-		l = n % 10;
-	
-
-		if (l > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, l);
-		}
-		else if (l == 0)
-		{
-			printf("Last digit of %d is %d and is 0\n", n, l);
-		}
-		else if (l < 6 && l != 0)
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-		}
-	
-
-		return (0);
+	putchar(c);
+	c = c + 1;
 	}
+	putchar('\n');
 
+	return (0);
+}
