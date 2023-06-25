fea7cfaa55ba8d3e21669f76d217aa0e63e0aa8e
root <ashayebisola@.com>
2023-06-24 18:19:54 -0700

    fg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Prints characters with printf
  *
@@ -6,6 +8,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
 }
