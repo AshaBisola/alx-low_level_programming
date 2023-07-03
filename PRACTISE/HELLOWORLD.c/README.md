1605a74d2c9451d0bcc96fb64326661aaf975cf8
root <ashayebisola@.com>
2023-07-03 11:47:17 -0700

    done

---
+++ PRACTISE/HELLOWORLD.c/README.md
@@ -0,0 +1,1 @@
+This directory contains my first event with  coding
---
+++ PRACTISE/HELLOWORLD.c/programhelloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints characters with printf
+ *
+ * program
+ *
+ * Return:0 succesful
+ */
+
+int main(void)
+{
+	printf("HelloWorld\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
